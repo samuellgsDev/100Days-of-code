ddb3936f71a22963c3f6884aff20eb03510c206f
samuellgsDev <samuellgs.dev@gmail.com>
2022-06-17 12:11:57 -0300

    1º dia

---
+++ BandNameGenerator.py
@@ -0,0 +1,8 @@
+# Create a program that generates a random band name.
+#
+# Crie um programa que gera um nome de banda aleatório.
+#
+print('Gerador de nomes de bandas')
+city_name = str(input('Digite o nome da cidade que você cresceu: '))
+pet_name = str(input('Digite o nome do seu pet: '))
+print('Seu nome de banda é: ' + city_name + ' ' + pet_name)
